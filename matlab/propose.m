5d190ef2fb3bdb1f73520161cdc47c336335f2a3
Bobbie Wu <boweiwu@umich.edu>
2019-04-24 13:17:15 -0400

    rename proposal func

--- matlab/propose.m
+++ matlab/propose.m
@@ -1,15 +1,16 @@
-function th = genSamp(th_t,model)
+function th = propose(th_t,model)
 % proposal algorithm, given the current theta_t, propose the next theta
-if nargin == 0, testGenSamp; return; end
+if nargin == 0, testPropose; return; end
 
 sig = model.sig_th/1.5; % std deviation for the proposal distr
+%sig = ones(size(th_t))*model.noise*10; % proposal distribution determined by noise_level
 th = mvnrnd(th_t,sig.^2,1);
 
-function testGenSamp
+function testPropose
 model.sig_th = 1;
 x = zeros(1,1000);
 for i = 1:1000
-    x(i) = genSamp(3,model); % normal dist rand num with mean 3
+    x(i) = propose(3,model); % normal dist rand num with mean 3
 end
 figure
 hist(x,50); % histogram to verify if normally distributed
